f04c4508eac820f6317a94390b84098dcb217414
RamEduard <ramon.calle.88@gmail.com>
2016-05-07 14:03:39 -0430

    Redefined constants.

--- src/MaterialDesignLiteModule/Resources/public/config/constants.js
+++ src/MaterialDesignLiteModule/Resources/public/config/constants.js
@@ -3,12 +3,12 @@
 
     angular
         .module('app')
-        .constant('API_URL', 'http://localhost/ramonserrano.com/web/api')
-        .constant('ASSETS_URL', 'http://localhost/ramonserrano.com/web/assets')
-        .constant('MODULES_URL', 'http://localhost/ramonserrano.com/web/modules')
-        .constant('CSS_URL', 'http://localhost/ramonserrano.com/web/modules/materialdesignlite/css')
-        .constant('IMAGES_URL', 'http://localhost/ramonserrano.com/web/modules/materialdesignlite/images')
-        .constant('JS_URL', 'http://localhost/ramonserrano.com/web/modules/materialdesignlite/js')
-        .constant('TEMPLATES_PATH', 'http://localhost/ramonserrano.com/web/modules/materialdesignlite/templates');
+        .constant('API_URL', 'http://ramonserrano.com/web/api')
+        .constant('ASSETS_URL', 'http://ramonserrano.com/web/assets')
+        .constant('MODULES_URL', 'http://ramonserrano.com/web/modules')
+        .constant('CSS_URL', 'http://ramonserrano.com/web/modules/materialdesignlite/css')
+        .constant('IMAGES_URL', 'http://ramonserrano.com/web/modules/materialdesignlite/images')
+        .constant('JS_URL', 'http://ramonserrano.com/web/modules/materialdesignlite/js')
+        .constant('TEMPLATES_PATH', 'http://ramonserrano.com/web/modules/materialdesignlite/templates');
 
 })();
